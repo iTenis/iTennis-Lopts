3eb7c71d07931390e10e3402971447576cabb339
iTenis <xiehuisheng@hotmail.com>
2019-01-13 18:56:40 -0800

    modify config and ipmi

--- tools/ipmi_ip_config.sh
+++ tools/ipmi_ip_config.sh
@@ -5,6 +5,7 @@ modprobe ipmi_poweroff
 modprobe ipmi_devintf
 modprobe ipmi_si
 ipmitool chassis power status
+ipmitool lan set 1 ipsrc static
 ipmitool lan set 1 ipaddr $1
 ipmitool lan set 1 netmask $2
 ipmitool lan set 1 defgw ipaddr $3
